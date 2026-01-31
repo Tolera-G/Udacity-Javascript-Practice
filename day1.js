595df22cdc08d3552177d0bcc9f50cf71d690410
tolera G <tolera@gmail.com>
2026-01-31 10:38:56 +0300

    if else statement

--- day1.js
+++ day1.js
@@ -5,3 +5,12 @@ let age = 19;
 
 console.log("Name:", name);
 console.log("Age:", age);
+/*Else if Statements */
+const weather="sunny";
+if(weather==="snow"){
+    console.log("Bring a coat.");
+}else if(weather==="rain"){
+    console.log("Bring a rain jacket.");
+}else{
+    console.log("wear what you have on.");
+}
