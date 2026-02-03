f406501e7df668168cefce4f3be7284455c5e672
tolera G <tolergirm@gmail.com>
2026-02-03 11:15:10 +0300

    changes.js

---
+++ Loops/changes.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Changing the Loop
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `x` with the value of `9`
+ * - Your `for` loop should have a stop condition for the value of `x`
+ * - Your `for` loop should decrement `x` each time it executes
+ * - Your code should produce the expected output
+ */
+ 
+// rewrite the while loop as a for loop
+
+for (let x = 9; x>=1; x--){
+    console.log("hello " + x);
+}
