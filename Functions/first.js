75de09333dc44f87455bcd95f4ff56fb5349bda8
Tolera G. <tolergirm@gmail.com>
2026-02-04 16:08:09 +0300

    the first.js

---
+++ Functions/first.js
@@ -0,0 +1,7 @@
+// your code goes here
+function laugh() {
+    // create a variable with any name of your choice
+    const sound = "hahahahahahahahahaha!"
+    return sound;
+}
+console.log(laugh());
