0045483a4eafe9d5a42f6591ab1343c38ab21fd2
Tolera G. <tolergirm@gmail.com>
2026-02-07 21:50:36 +0300

    serenity

---
+++ ARRAYS/serenity.js
@@ -0,0 +1,12 @@
+const captain = "Mal";
+const second = "Zoe";
+const pilot = "Wash";
+const companion = "Inara";
+const mercenary = "Jayne";
+const mechanic = "Kaylee";
+
+// Create the 'crew' array containing the variables above
+const crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+// Print the crew array to the console
+console.log(crew);
