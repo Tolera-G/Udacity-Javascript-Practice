7eedd65e04e8694861a8223075eddb1f605ae002
Tolera G. <tolergirm@gmail.com>
2026-02-07 22:42:05 +0300

    rainbow

---
+++ ARRAYS/rainbow.js
@@ -0,0 +1,22 @@
+/*
+ * Programming Quiz: Colors of the Rainbow (6-4)
+ *
+ * Use only the splice() method to modify the rainbow variable:
+ *  - remove "Blackberry"
+ *  - add "Yellow" and "Green"
+ *  - add "Purple"
+ */
+
+
+const rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
+
+// remove "Blackberry" at index 2
+rainbow.splice(2, 1);
+
+// add "Yellow" and "Green" starting at index 2
+rainbow.splice(2, 0, 'Yellow', 'Green');
+
+// add "Purple" at the end (index 5 after previous insertions)
+rainbow.splice(5, 0, 'Purple');
+
+console.log(rainbow);
