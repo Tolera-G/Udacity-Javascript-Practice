661010977682fee8b1eccfdf1a2c78bc2350a8c5
Tolera G. <tolergirm@gmail.com>
2026-02-07 22:59:03 +0300

    join

---
+++ ARRAYS/join.js
@@ -0,0 +1,17 @@
+const captain = "Mal";
+const second = "Zoe";
+const pilot = "Wash";
+const companion = "Inara";
+const mercenary = "Jayne";
+const mechanic = "Kaylee";
+
+const crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+const doctor = "Simon";
+const sister = "River";
+const shepherd = "Book";
+
+// add the new members
+crew.push(doctor, sister, shepherd);
+
+console.log(crew);
