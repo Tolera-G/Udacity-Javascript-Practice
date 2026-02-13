c81398b50adee77e442737233158026dbe6c5712
Tolera G. <tolergirm@gmail.com>
2026-02-14 00:14:02 +0300

    menu

---
+++ Objects/menu.js
@@ -0,0 +1,11 @@
+// Breakfast object
+var breakfast = {
+  name: "The Lumberjack",
+  price: 9.95,
+  ingredients: ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
+};
+
+// Example usage:
+console.log(breakfast.name);        // The Lumberjack
+console.log(breakfast.price);       // 9.95
+console.log(breakfast.ingredients); // ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
